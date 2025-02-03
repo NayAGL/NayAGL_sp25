2b01eeacc6e41ed7b4b7ffc927c7e02d47c34412
nicho <nicho@NIX2D>
2025-02-03 11:30:20 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace NayAGL_sp25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
